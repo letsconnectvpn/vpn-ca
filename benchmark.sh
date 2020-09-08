4ab3547f1b0627f25b3f373ba5c4785a558474a6
François Kooman <fkooman@tuxed.net>
2020-09-08 13:22:34 +0200

    add benchmark script

---
+++ benchmark.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+# Generate 50 keys and have them signed by the CA and time how long this takes
+# for RSA, ECDSA and EdDSA.
+#
+# On my oldish laptop:
+#
+# $ sh benchmark.sh 
+# RSA 53s
+# ECDSA 1s
+# EdDSA 0s
+
+for TYPE in RSA ECDSA EdDSA; do
+	mkdir "${TYPE}"
+	CA_DIR="${TYPE}" CA_KEY_ALGO="${TYPE}" _bin/vpn-ca -init-ca -name "${TYPE} CA"
+	START=$(date +%s)
+	for i in $(seq 50); do
+        	CA_DIR="${TYPE}" CA_KEY_ALGO="${TYPE}" _bin/vpn-ca -client -name "${TYPE}-client-${i}"
+	done
+	END=$(date +%s)
+	echo "${TYPE} $((END-START))s"
+	rm -rf "${TYPE}"
+done
